21b0cd79edb5a11821f4dfc7f9a866c7f5af9a0e
daqhris <817573+daqhris@users.noreply.github.com>
2026-06-09 15:46:59 +0200

    Cleanup stale files from MkDocs build pipeline

--- docs/gallery.md
+++
@@ -1,8 +0,0 @@
----
-title: gallery
----
-
-![](images/image-02.jpg)
-![](images/image-01.jpg)
-![](images/image-03.jpg)
-
--- main.py
+++
@@ -1,9 +0,0 @@
-
-def define_env(env):
-    print("✅ main.py chargé")
-
-    @env.macro
-    def test_macro():
-        return "macro OK"
-
-
